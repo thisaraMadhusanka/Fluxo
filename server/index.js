bc60717a4f6dcdfe7e06f376d307fe858a8ccf16
Thisara Madhusanka <thisarasanka4@gmail.com>
2026-01-07 14:35:06 +0530

    Hardcode CORS origin

--- server/index.js
+++ server/index.js
@@ -10,7 +10,7 @@ const PORT = process.env.PORT || 5000;
 
 // Middleware
 app.use(cors({
-    origin: [process.env.CLIENT_URL, 'http://localhost:5173', 'http://localhost:3000'],
+    origin: ['https://fluxo-xi.vercel.app', 'http://localhost:5173', 'http://localhost:3000'],
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS']
 }));
